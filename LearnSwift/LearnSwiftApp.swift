dd241b02b3c05e80ae5d356e7969c66082f701ac
hanum <rntls123@naver.com>
2022-02-23 23:20:37 +0900

    First commit

---
+++ LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by 오한음 on 2022/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
